9920134f3f4994148ab8745da8cbc30fd0568a90
myriam-Elkouby <essayagmyriam>
2024-01-22 09:47:51 +0200

    first commit

---
+++ client/src/service.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+
+// Config default base URL API requests
+const apiUrl = "https://localhost:5170";
+axios.defaults.baseURL = apiUrl;
+// axios.defaults.headers.common = {
+//   'Content-Type': 'application/json',
+//   'Access-Control-Allow-Origin': '*',
+//   //'Access-Control-Allow-Credentials': 'true',
+//   'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+//   'Access-Control-Allow-Headers': 'Content-Type, Depth, User'
+// };
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+const writeToLog = (error) => {
+  console.error('Error :', error);
+};
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {   
+    writeToLog(error);
+    return Promise.reject(error);
+  }
+);
+
+export default {
+  getTasks: async () => {
+    try{
+      const result = await axios.get(`${apiUrl}/items`,{ crossDomain: true });    
+      return result.data;
+    }
+    catch (error) {
+      console.error('Error in getTasks:', error);
+      throw error; }
+
+  },
+
+  addTask: async(name)=>{
+    console.log('addTask', name)
+    try{
+    const result = await axios.post(`${apiUrl}/items?name=${name}`, { crossDomain: true });  
+    return result.data;
+    }
+    catch (error) {  
+      console.error('Error in addTask:', error);
+      throw error; 
+    }
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    console.log('setCompleted', {id, isComplete})
+    try{
+      var result = await axios.put(`${apiUrl}/items/${id}?complete=${isComplete}`,{ crossDomain: true });
+      return result;
+    }
+    catch (error) {
+      console.error('Error in setCompleted:', error);
+      throw error; 
+    }
+  },
+
+  deleteTask:async(id)=>{
+    console.log('deleteTask')
+    try{
+      var result = await axios.delete(`${apiUrl}/items/${id}`,{ crossDomain: true });
+      return result;
+    }
+    catch(error){
+      console.error('Error in deleteTask:', error);
+      throw error;
+    }
+  }
+};
---
+++ server/Program.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using TodoApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+//how to do a dependency injection?
+builder.Services.AddDbContext<ToDoDbContext>();
+
+builder.Services.AddSwaggerGen();
+
+var  AllowReactOrigins = "ReactOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowReactOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000")
+                          .AllowAnyMethod()
+                            .AllowAnyHeader();
+                      });
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseCors(AllowReactOrigins);
+
+
+//getTasks
+
+app.MapGet("/items", async(ToDoDbContext context) => {
+    var items = await context.Items.ToListAsync();
+    return Results.Ok(items);
+});
+
+//addTask
+app.MapPost("/items", async (string name,ToDoDbContext context) => {   
+    Item item = new Item { Name = name, IsComplete = false };
+    context.Items.Add(item);
+    await context.SaveChangesAsync();
+    return Results.Ok(item);
+});
+
+//setCompleted
+app.MapPut("/items/{id:int}", async(int id, bool complete,ToDoDbContext context) => {
+    var item = await context.Items.FindAsync(id);
+    if (item == null)
+    {
+        return Results.NotFound();
+    }
+    item.IsComplete = complete;
+    await context.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+//deleteTask
+app.MapDelete("/items/{id:int}", async(int id,ToDoDbContext context) => {
+    var item = await context.Items.FindAsync(id);
+    if(item == null){
+        return Results.NotFound();
+    }  
+    context.Items.Remove(item);
+    await context.SaveChangesAsync(); 
+    return Results.NoContent();
+});
+
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    //app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.Run("https://localhost:5170");
+
+
+
+//https://localhost:5170
